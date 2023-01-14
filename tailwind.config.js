ee351edbcd6b2b434280abe9f86e08c7f6d0accc
Naufal05R <naufalrabbaniishere@gmail.com>
2023-01-14 22:06:42 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        'blob': '390px',
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
